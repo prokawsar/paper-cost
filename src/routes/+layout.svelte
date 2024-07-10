ed523eb6ad40b7fba635cf7376e73e05a5d8fcaf
prokawsar <prokawsar@gmail.com>
2024-07-10 10:04:29 +0600

    Done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,14 @@
 	import '@fontsource/jost'
 </script>
 
-<nav>
-	<div class="flex justify-center py-2">
-		<p class="text-center text-3xl font-semibold">Molla Printing & Packaging</p>
-	</div>
-	<div
-		class="w-full max-w-6xl mx-auto bg-gradient-to-r from-transparent via-orange-800/40 to-transparent p-[1px]"
-	/>
-</nav>
-<slot />
+<main class="h-[100svh] flex flex-col">
+	<nav>
+		<div class="flex justify-center py-2">
+			<p class="text-center text-3xl font-semibold">Molla Printing & Packaging</p>
+		</div>
+		<div
+			class="w-full max-w-6xl mx-auto bg-gradient-to-r from-transparent via-orange-800/40 to-transparent p-[1px]"
+		/>
+	</nav>
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,72 +44,79 @@
 	}
 
 	const fields = Object.keys(paperFields).filter((key) => key !== 'id')
+
+	$: paperCount.length == 0 ? clearAll() : ''
 </script>
 
 <svelte:head>
 	<title>Paper calculator</title>
 </svelte:head>
 
-<section class="max-w-6xl mx-auto flex w-full flex-col gap-4 p-5">
+<section class="max-w-6xl mx-auto flex w-full flex-col gap-4 px-4 py-5 h-[90%]">
 	<h1 class="text-2xl text-center">Paper Cost</h1>
 	<div class="w-full bg-gradient-to-r from-transparent via-slate-600/10 to-transparent p-[1px]" />
-	<div class="flex flex-col w-full gap-4 items-center">
-		{#each paperCount as paper, i}
-			<div class="flex flex-col gap-1 items-center">
-				<div class="flex flex-row justify-between w-full">
-					<p class="font-bold w-fit">
-						Paper {i + 1}
-					</p>
+	<div class="flex flex-col w-full justify-between h-[90%] items-center">
+		<div class="flex flex-col gap-4 h-full overflow-auto">
+			{#each paperCount as paper, i}
+				<div class="flex flex-col gap-1 items-center">
+					<div class="flex flex-row justify-between w-full">
+						<p class="font-bold w-fit">
+							Paper {i + 1}
+						</p>
 
-					<button
-						class="border border-gray-200 rounded-md p-2 text-red-400 w-fit"
-						on:click={() => removePaper(paper.id)}
-					>
-						<Icon icon="ph:trash-light" width="16px" />
-					</button>
+						<button
+							class="border border-gray-200 rounded-md p-2 text-red-400 w-fit"
+							on:click={() => removePaper(paper.id)}
+						>
+							<Icon icon="ph:trash-light" width="16px" />
+						</button>
+					</div>
+					<div class="grid grid-cols-4 w-full gap-1 px-1">
+						{#each fields as field}
+							<Input bind:value={paper[field]} placeholder={field} />
+						{/each}
+					</div>
 				</div>
-				<div class="grid grid-cols-4 w-full gap-1">
-					{#each fields as field}
-						<Input bind:value={paper[field]} placeholder={field} />
-					{/each}
-				</div>
-			</div>
-		{/each}
-		<!-- Calculate result section -->
-		<div class="font-bold text-lg flex w-full">
-			{#if finalPrice}
-				<Result total={finalPrice} />
-			{/if}
+			{/each}
 		</div>
 
-		<!-- Button section -->
-		<div
-			class:justify-between={paperCount.length}
-			class="flex flex-row justify-center w-full max-w-3xl"
-		>
-			<button
-				disabled={paperCount.length == MAX_PAPER}
-				class="border border-slate-300 rounded-md text-sm px-3 py-1 w-fit disabled:text-slate-400"
-				on:click={addPaper}
+		<!-- Button and result section -->
+		<div class="flex flex-col justify-center max-w-3xl w-full gap-4">
+			<!-- result section -->
+			<div class="font-bold text-lg flex w-full">
+				{#if finalPrice}
+					<Result total={finalPrice} />
+				{/if}
+			</div>
+			<!-- button section -->
+			<div
+				class:justify-between={paperCount.length}
+				class="flex flex-row justify-center w-full mt-3"
 			>
-				Add paper
-			</button>
-			{#if finalPrice}
 				<button
-					class="border border-red-200 rounded-md px-3 py-1 text-red-400 w-fit"
-					on:click={clearAll}
+					disabled={paperCount.length == MAX_PAPER}
+					class="border border-slate-300 rounded-md text-sm px-3 py-1 w-fit disabled:text-slate-400"
+					on:click={addPaper}
 				>
-					Clear
+					Add paper
 				</button>
-			{/if}
-			{#if paperCount.length}
-				<button
-					class="border font-semibold border-gray-200 rounded-md px-3 py-1 text-teal-600 w-fit"
-					on:click={calculatePaperCost}
-				>
-					Calculate
-				</button>
-			{/if}
+				{#if finalPrice}
+					<button
+						class="border border-red-200 rounded-md px-3 py-1 text-red-400 w-fit"
+						on:click={clearAll}
+					>
+						Clear
+					</button>
+				{/if}
+				{#if paperCount.length}
+					<button
+						class="border font-semibold border-gray-200 rounded-md px-3 py-1 text-teal-600 w-fit"
+						on:click={calculatePaperCost}
+					>
+						Calculate
+					</button>
+				{/if}
+			</div>
 		</div>
 	</div>
 </section>
