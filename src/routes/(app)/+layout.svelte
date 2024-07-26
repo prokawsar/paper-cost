6fab4ea5e88b57038e51562882631fc329d8027b
Kawsar Ahmed <prokawsar@gmail.com>
2024-07-26 17:28:35 +0600

    Feat/footer menu (#4)
    * .env example
    
    * Menu in footer
    
    * Footer menu
    
    * uncomment

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -9,6 +9,7 @@
 	import { slide } from 'svelte/transition'
 	import { supabase } from '$lib/db/supabaseClient'
 	import { Toaster } from 'svelte-sonner'
+	import Icon from '@iconify/svelte'
 
 	//Import Mixpanel SDK
 	mixpanel.init(PUBLIC_MIX_TOKEN, {
@@ -24,11 +25,11 @@
 	}
 </script>
 
-<main class="h-[100svh] flex flex-col">
+<main class="h-[100svh] flex flex-col justify-between">
 	<Toaster
 		richColors
 		toastOptions={{
-			class: 'py-2'
+			class: 'py-2 mb-8'
 		}}
 	/>
 	{#if $navigating}
@@ -38,35 +39,52 @@
 			<Loader />
 		</div>
 	{/if}
-	<nav class="max-w-6xl mx-auto w-full">
-		<div class="flex justify-center py-2">
-			<p class="text-center text-2xl text-red-600 font-semibold">Molla Printing & Packaging</p>
-		</div>
-		<div class="bg-gradient-to-r from-transparent via-orange-800/40 to-transparent p-[1px]" />
-		<div class="px-4 flex w-full mt-2">
-			<div class="flex w-full flex-row justify-end gap-3 py-1 px-2 rounded border border-teal-400">
-				<a href="/history" class:hidden={$page.url.pathname == '/history'} class="h-full">History</a
-				>
-				<a href="/" class:!block={$page.url.pathname == '/history'} class="hidden h-full">Home</a>
+	<div class="flex flex-col h-[92%]">
+		<nav class="max-w-6xl mx-auto w-full">
+			<div class="flex justify-center py-2">
+				<p class="text-center text-2xl text-red-600 font-semibold">Molla Printing & Packaging</p>
 			</div>
-		</div>
-	</nav>
-	<slot />
+			<div class="bg-gradient-to-r from-transparent via-orange-800/40 to-transparent p-[1px]" />
+		</nav>
+		<slot />
+	</div>
 
-	<div class="absolute bottom-0 w-full flex">
-		<p class="text-right md:text-center text-gray-400 w-3/4">
+	<!-- Footer menu section -->
+	<div
+		class="max-w-6xl mx-auto w-full flex flex-row h-11 px-3 bg-slate-50 border-t border-teal-500 items-center justify-between rounded-t-lg"
+	>
+		<!-- <p class="text-right md:text-center text-gray-400 w-3/4">
 			&#x1F4BB;Developed by <a href="https://github.com/prokawsar" target="_blank">ProKawsar</a
 			>&#x1F60E;
-		</p>
-		<!-- <button on:click={() => (showSettings = !showSettings)} class="w-1/4"> Settings </button> -->
-		{#if showSettings}
-			<div
-				transition:slide={{ axis: 'y', duration: 200 }}
-				class="absolute right-2 bottom-7 flex flex-col items-start divide-y divide-orange-400 gap-1 bg-slate-100 p-2 rounded"
-			>
-				<button on:click={() => handleLogout()}>Logout</button>
-				<button>Change pin</button>
-			</div>
-		{/if}
+		</p> -->
+		<a
+			href="/"
+			class="flex flex-row items-center gap-1"
+			class:text-orange-500={$page.url.pathname == '/'}
+		>
+			<Icon icon="clarity:home-line" /> Home
+		</a>
+		<a
+			href="/history"
+			class="flex flex-row items-center gap-1"
+			class:text-orange-500={$page.url.pathname == '/history'}
+		>
+			<Icon icon="ph:read-cv-logo-light" /> Cost History
+		</a>
+		<button
+			on:click={() => (showSettings = !showSettings)}
+			class="flex flex-row gap-1 items-center"
+		>
+			<Icon icon="solar:settings-linear" /> Settings
+		</button>
 	</div>
+	{#if showSettings}
+		<div
+			transition:slide={{ axis: 'y', duration: 200 }}
+			class="absolute w-20 right-2 bottom-11 flex flex-col items-start divide-y divide-orange-400 gap-1 bg-slate-50 p-2 rounded"
+		>
+			<!-- <button on:click={() => handleLogout()}>Logout</button> -->
+			<button>About</button>
+		</div>
+	{/if}
 </main>
