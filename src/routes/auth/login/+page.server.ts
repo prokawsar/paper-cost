1fd1710f8c9bae0eda5da6431ff57483749db1a9
Kawsar Ahmed <prokawsar@gmail.com>
2024-08-29 08:49:29 +0600

    remove console log

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -19,7 +19,7 @@ export const actions: Actions = {
 		const formData = await request.formData()
 		const email = formData.get('email') as string
 		const password = formData.get('password') as string
-		console.log(email, password)
+
 		const { error } = await supabase.auth.signInWithPassword({ email, password })
 		if (error) {
 			console.error(error)
